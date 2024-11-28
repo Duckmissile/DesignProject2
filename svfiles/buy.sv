6f0506af1d95ea31af9079241df38bd60e6c0980
Noah Unger-Schulz <noahungerschulz@gmail.com>
2024-11-28 03:40:22 -0800

    adds stock so then you can only have limited items untested tis too late

--- svfiles/buy.sv
+++ svfiles/buy.sv
@@ -3,13 +3,17 @@ module buy (input logic Reset,
             input logic [3:0] Buy,
             output logic [6:0] C,
             output logic [3:0] Vending);
-    logic [3:0] CanBuy; 
+    logic [3:0] CanBuy,S0,S1,S2,S3; 
     logic [6:0] C0,C1,C2,C3;
-    canbuy  #(.cost(5)) cb0 (Money,CanBuy[0]);
-    canbuy  #(.cost(10)) cb1 (Money,CanBuy[1]);
-    canbuy  #(.cost(15)) cb2 (Money,CanBuy[2]);
-    canbuy  #(.cost(30)) cb3 (Money,CanBuy[3]);
+    canbuy  #(.cost(5)) cb0 (Money,S0,CanBuy[0]);
+    canbuy  #(.cost(10)) cb1 (Money,S1,CanBuy[1]);
+    canbuy  #(.cost(15)) cb2 (Money,S2,CanBuy[2]);
+    canbuy  #(.cost(30)) cb3 (Money,S3,CanBuy[3]);
     assign Vending = Buy & CanBuy;
+    ff fs0 (Vending[0],Reset,1,S0,S0-1);
+    ff fs1 (Vending[1],Reset,1,S1,S1-1);
+    ff fs2 (Vending[2],Reset,1,S2,S2-1);
+    ff fs3 (Vending[3],Reset,1,S3,S3-1);
     fastcounter #(.cost(-5)) fsc0 (Vending[0],Reset,C0);
     fastcounter #(.cost(-10)) fsc1 (Vending[1],Reset,C1);
     fastcounter #(.cost(-15)) fsc2 (Vending[2],Reset,C2);
--- svfiles/canbuy.sv
+++ svfiles/canbuy.sv
@@ -1,6 +1,7 @@
 //checks if there is enough time to buy something of cost
 module canbuy #(parameter cost=5) (
-                input logic [:0] money,
+                input logic [6:0] money,
+                input logic [3:0] stock,
                 output logic ans);
-    assign ans = (money>cost);
+    assign ans = (money>cost) & (stock>0);
 endmodule
