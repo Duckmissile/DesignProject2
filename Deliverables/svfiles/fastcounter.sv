41df83b23b89e3c6b35b5a3b7d3ae65824f9c1e9
Noah Unger-Schulz <noahungerschulz@gmail.com>
2024-12-02 15:16:48 -0800

    moved svfiles and dofiles in Deliverables

--- Deliverables/svfiles/fastcounter.sv
+++ Deliverables/svfiles/fastcounter.sv
@@ -1,9 +1,9 @@
 //this is a counter that resets after N tics
 module fastcounter #(parameter cost=5) (input logic clk,
                 input logic reset,
-                output logic [7:0] tics);
+                output logic [6:0] tics);
     //asynchronous reset
     always_ff@(posedge clk,posedge reset)
-        if (reset)   tics<=8'b0;
+        if (reset)   tics<=7'b0;
         else tics<=tics+cost;
 endmodule
